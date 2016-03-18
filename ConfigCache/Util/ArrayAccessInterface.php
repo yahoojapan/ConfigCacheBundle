6249cb22e5e3d4067906d89ca2596908366e2ae1
msmsny <smishima@yahoo-corp.jp>
2016-03-18 15:08:26 +0900

    Update header comments

--- ConfigCache/Util/ArrayAccessInterface.php
+++ ConfigCache/Util/ArrayAccessInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the ConfigCacheBundle package.
  *
- * Copyright (c) 2015 Yahoo Japan Corporation
+ * Copyright (c) 2015-2016 Yahoo Japan Corporation
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
